f5c6e73885d8efc5df8a824a78acbddcef4a22ef
tuan-shark <tuannm.23bi14441@usth.edu.vn>
2025-06-16 13:18:04 +0700

    Add files via upload
    update all Part 1

---
+++ Java_Basics/operators/arithmatic_operators.java
@@ -0,0 +1,17 @@
+package Java_Basics.operators;
+
+public class arithmatic_operators {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 15;
+        System.out.println(a+b);
+        System.out.println(a*b);
+        System.out.println(a-b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+        System.out.println(a++);
+        System.out.println(++a);
+        System.out.println(a--);
+        System.out.println(--a);
+    }
+}
